25ded35bc8bb43be34206d1361ce0a62680a038f
토자맨 <zzoon5020@naver.com>
2023-07-13 16:00:29 +0900

    feat: 기본 면접 추가

--- speak_to_chat/views.py
+++ speak_to_chat/views.py
@@ -9,7 +9,7 @@ import openai
 import base64
 from .tasks import process_whisper_data
 from django.core.files.base import ContentFile
-from .models import Answer
+from .models import Answer, Question
 import os
 from .serializers import ResponseVoiceSerializer
 from django.core.files.temp import NamedTemporaryFile
@@ -22,6 +22,7 @@ from .models import Question
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.exceptions import AuthenticationFailed
+import random
 
 load_dotenv()
 openai.api_key = os.getenv("GPT_API_KEY")
@@ -30,135 +31,143 @@ openai.api_key = os.getenv("GPT_API_KEY")
 # 기본 인터뷰
 class DefaultInterview(APIView):
     parser_classes = [MultiPartParser]
-
-    # 처음 데이터를 받아야 하는 경우 -> 음성 데이터는 없음. 그냥 GPT 질문 시작.
-    @swagger_auto_schema(responses={"200": ResponseVoiceSerializer})
+    # 답변과 상관없이 질문을 랜덤으로 뽑아서 지원자에게 질문
+    @swagger_auto_schema(
+        operation_description="심층 면접 데이터 최초 받기",
+        operation_id="form_id를 입력해주세요.",
+        manual_parameters=[
+            openapi.Parameter(
+                name="form_id",
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_INTEGER,
+                required=True,
+                description="form_id",
+            )
+        ],
+        responses={"200": ResponseVoiceSerializer},
+    )
     def get(self, request):
-        self.conversation = []
+        
+        # 랜덤으로 기본 질문 1개 뽑기
+        message = self.pick_random_question()
+        
+        # form_id 받기, 파라미터로 받기
+        form_id = request.GET.get("form_id")
+        
+        # form_id에 해당하는 Form 객체 생성
+        form_object = Form.objects.get(id=form_id)
 
-        # 대화 시작 메시지 추가
-        self.conversation.append(
-            {
-                "role": "user",
-                "content": "Let's do a job interview simulation. You're an interviewer and I'm an applicant. When I ask you to start taking interview, then start asking questions. I will say the phrase “start the interview” for you to start. Ask one question at a time. Then, ask another question after I provided the answer. Continue this process until I ask you to stop. Please say 'Yes' if you understood my instructions.",
-            }
-        )
-        self.conversation.append(
-            {
-                "role": "assistant",
-                "content": "Yes. I am playing the role of the interviewer. I'll do my best.",
-            }
-        )
-        self.conversation.append(
-            {
-                "role": "user",
-                "content": "You can only ask me some questions from now on. Don't say anything other than a question. Please just say 'Explain the applicant.' if you understood my instructions.",
-            }
-        )
-        self.conversation.append(
-            {"role": "assistant", "content": "Explain the applicant."}
-        )
-        self.conversation.append(
-            {
-                "role": "user",
-                "content": "Start the interview. I will apply to Naver as a front-end developer. Also, Start the interview. I will apply to Naver as a front-end developer. Remember. I am applying for the first time in this field. ",
-            },
-        )
-
-        self.conversation.append({"role": "assistant", "content": "ok. I understand."})
-
-        self.conversation.append(
-            {
-                "role": "user",
-                "content": "please ask me the questions one at a time. And answer only the question you created by translating them into Korean. Don't answer in English.",
-            }
-        )
-
-        self.conversation.append({"role": "assistant", "content": "ok. I understand."})
-
-        self.conversation.append(
-            {
-                "role": "user",
-                "content": "Please prepare challenging and common question for interviewers when applying for Naver Front Developer.",
-            }
-        )
-
-        # 대화 계속하기
-        message = self.continue_conversation()
-
+        # Question 테이블에 Form 객체 데이터 추가
+        # form_id=form_object : Question의 외래키를 추가하여 연결시킴
+        Question.objects.create(content=message, form_id=form_object)
+        
         return Response(message, status=status.HTTP_200_OK)
-
+    
     # 음성 데이터를 받아야 하는 경우
     @swagger_auto_schema(
-        operation_description="Upload container excel, if the columns and data are valid. Containers will be created. "
-        "If container with such name already exists, it will be update instead",
-        operation_id="Upload container excel",
+        # API 작업에 대한 설명
+        operation_description="음성 데이터 POST",
+        # 작업의 고유 식별자
+        operation_id="음성 파일을 업로드 해주세요.",
+        # voice_file 파일을 업로드하는 매개변수를 정의
         manual_parameters=[
+            openapi.Parameter(
+                name="form_id",
+                in_=openapi.IN_FORM,
+                type=openapi.TYPE_INTEGER,
+                required=True,
+                description="지원 정보 아이디",
+            ),
+            openapi.Parameter(
+                name="question_id",
+                in_=openapi.IN_FORM,
+                type=openapi.TYPE_INTEGER,
+                required=True,
+                description="질문 아이디",
+            ),
             openapi.Parameter(
                 name="voice_file",
                 in_=openapi.IN_FORM,
                 type=openapi.TYPE_FILE,
                 required=True,
-                description="Document",
-            )
+                description="음성 데이터",
+            ),
         ],
+        # API 응답에 대한 설명
         responses={400: "Invalid data in uploaded file", 200: "Success"},
     )
+    # @action : Django ViewSet에서 사용되며, 특정 작업에 대한 추가적인 액션을 정의할 때 사용
     @action(
         detail=False,
         methods=["post"],
         parser_classes=(MultiPartParser,),
-        name="upload-excel",
-        url_path="upload-excel",
+        name="upload-voice-file",
+        url_path="upload-voice-file",
     )
     def post(self, request, format=None):
         self.conversation = []
 
-        # 답변을 받고, 응답을 해주는 부분 -> 음성 파일 추출 필요
-        # 음성 파일을 추출하고, wav 파일로 저장 -> 오디오를 텍스트로 변환.
+        # 음성 답변 받기
         audio_file = request.FILES["voice_file"]
-
-        print(audio_file)
+        form_id = request.data["form_id"]
+        question_id = request.data["question_id"]
+        # 음성 -> 텍스트
         transcript = openai.Audio.transcribe("whisper-1", audio_file)
-        transcription = transcript["text"]
-
-        # self.conversation.append(
-        #     {
-        #         "role": "user",
-        #         "content": "Let's do a job interview simulation. You're an interviewer and I'm an applicant. When I ask you to start taking interview, then start asking questions. I will say the phrase “start the interview” for you to start. Ask one question at a time. Then, ask another question after I provided the answer. Continue this process until I ask you to stop. Please say 'Yes' if you understood my instructions.",
-        #     }
-        # )
-        # self.conversation.append(
-        #     {
-        #         "role": "assistant",
-        #         "content": "Yes. I am playing the role of the interviewer. I'll do my best.",
-        #     }
-        # )
-        # self.conversation.append(
-        #     {
-        #         "role": "user",
-        #         "content": "You can only ask me some questions from now on. Don't say anything other than a question. Please just say 'Explain the applicant.' if you understood my instructions.",
-        #     }
-        # )
-        # self.conversation.append(
-        #     {"role": "assistant", "content": "Explain the applicant."}
-        # )
-        # self.conversation.append(
-        #     {
-        #         "role": "user",
-        #         "content": "Start the interview. I will apply to Naver as a front-end developer. Also, I am a new front-end applicant. Please prepare challenging and common questions for interviewers when applying for Naver Front Developer. If I answer, tell me okay and present me the next question you prepared earlier. Now ask the question.",
-        #     },
-        # )
-
+        transcription = transcript['text']
+        
+        # 
+        question_object = Question.objects.get(question_id=question_id)
+        # Answer.content에 답변 저장
+        Answer.objects.create(content=transcription, question_id=question_object)
+        
+        # 랜덤으로 질문 1개 뽑기
+        message = self.pick_random_question()
+        
+        # id=form_id인 Form 객체 가져오기(get)
+        form_object = Form.objects.get(id=form_id)
+        
+        Question.objects.create(content=message, form_id=form_object)
+        
+        # Answer.objects.create(content=transcription, question_id=question_id, recode_file='s3_주소')
+        
+        
         # Response객체를 생성하여 데이터와 상태 코드 반환
         return Response(transcription, status=status.HTTP_200_OK)
-
-    def continue_conversation(self):
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=self.conversation, temperature=0.9, n=1
-        )
-
-        message = completion.choices[0].message["content"]
+    
+    # 질문을 랜덤으로 뽑는 함수
+    def pick_random_question(self):
+        
+        # 중복 질문이 나오면 다시 뽑음 (그냥 삭제하는 걸로 할까?)
+        pick_question = []
+        while True:
+            basic_questions_list = [
+                "우리 회사에 지원한 동기가 무엇입니까?",
+                "자신의 장점과 단점에 대해 이야기해보세요.",
+                "최근에 읽은 책이나 영화는 무엇입니까?",
+                "본인의 취미나 특기가 무엇입니까?",
+                "자신만에 스트레스 해소법은 무엇입니까?",
+                "5년 뒤, 10년 뒤 자신의 모습이 어떨 것 같습니까?",
+                "가장 존경하는 인물은 누구입니까?",
+                "본인이 추구하는 가치나 생활신조, 인생관, 좌우명은 무엇입니까?",
+                "자기 계발을 위해 무엇을 합니까?",
+                "취업기간에 무엇을 하셨나요?",
+                "가장 기억에 남는 갈등 경험을 말해주세요",
+                "가장 필요한 역량은 무엇이라 생각하나요?",
+                "우리 회사의 단점이 무엇이라고 생각하나요?",
+                "성취를 이룬 경험이 있나요? 그 경험을 설명해주세요.",
+                "과거에 어떤 도전적인 상황을 겪었으며, 그 상황에서 어떻게 대응했나요?",
+                
+                
+                ]
+            
+            message = random.choice(basic_questions_list)
+            
+            if message in pick_question:
+                continue
+            
+            pick_question.append(message)
+            break
+        
         return message
 
 
