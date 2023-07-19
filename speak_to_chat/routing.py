575eff3960e614df54cba6f9f96eb5e389f143d6
토자맨 <zzoon5020@naver.com>
2023-07-19 13:35:01 +0900

    refeator: web socket 방식으로 migration(question, answer object 추가)

--- speak_to_chat/routing.py
+++ speak_to_chat/routing.py
@@ -1,7 +1,9 @@
 from django.urls import path, re_path
 from .deep_interview_consumer import DeepInterviewConsumer
+from .default_interview_consumer import DefaultInterviewConsumer
 
 
 websocket_urlpatterns = [
     re_path(r"ws/deep-interview/$", DeepInterviewConsumer.as_asgi()),
+    re_path(r"ws/default-interview/$", DeepInterviewConsumer.as_asgi()),
 ]
