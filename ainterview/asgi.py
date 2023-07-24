07162ee0da3cc55dd44f43b5f970ebb64911d70f
hsjni0110 <hansukjinid@gmail.com>
2023-07-24 19:51:47 +0900

    fix: socket 하나만 쓰는 걸로 수정

--- ainterview/asgi.py
+++ ainterview/asgi.py
@@ -12,7 +12,6 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from speak_to_chat.deep_interview_consumer import DeepInterviewConsumer
 from channels.auth import AuthMiddlewareStack
 from django.urls import re_path
 import speak_to_chat.routing
